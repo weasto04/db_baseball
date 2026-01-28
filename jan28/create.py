4f7bc27235bbae9c519f5e981e96074f27d31cd8
Whitley Easton <weasto04@lakers.mercyhurst.edu>
2026-01-28 15:47:37 +0000

    first commit

---
+++ jan28/create.py
@@ -0,0 +1,9 @@
+import sqlite3
+import pandas as pd 
+
+conn = sqlite3.connect('baseball.db')
+cursor = conn.cursor()
+df = pd.read_csv('batting.csv')
+df.to_sql('batting', conn, if_exists = 'replace', index = False)
+conn.commit()
+conn.close()
---
+++ jan28/fetch.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd 
+
+conn = sqlite3.connect('baseball.db')
+cursor = conn.cursor()
+query = """
+    SELECT playerID, teamID, yearID, HR
+    FROM batting
+    WHERE teamID = 'PHI' AND yearID = 1976 AND HR != 0
+    ORDER BY HR desc;
+"""
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+records_df = pd.DataFrame(records, columns = ["playerID", "teamID", "yearID", "HR"])
+
+print(records_df)
+
